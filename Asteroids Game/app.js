d05b6d7e4148809a48c0dde6d325f05222669e09
nostprumprajee <nostprumprajee@gmail.com>
2019-01-24 14:59:10 +0700

    edit asteriods

--- Asteroids Game/app.js
+++ Asteroids Game/app.js
@@ -1,25 +1,36 @@
 /*eslint-disable */
 var ship
+var asteroids = []
 function setup () {
   createCanvas(windowWidth, windowHeight) // สร้าง container เพื่อทำการเเสดงผล
   ship = new Ship()
+  for (var i = 0; i < 5; i++) {
+    asteroids.push(new Asteroids())
+  }
 }
 
 function draw () {
   background(51)
   ship.render() // เรียกการทำงานของ function this.render
+  ship.turn()
+  ship.update()
+  ship.edges()
+
+  for (var i = 0; i < asteroids.length; i++) {
+    asteroids[i].render()
+    asteroids[i].update()
+    asteroids[i].edges()
+  }
 }
 
 
-// class
+function keyReleased () {
+  ship.setRotation(0)
+  ship.boosting(false)
+}
 
-function Ship () {
-  this.pos = createVector(width / 2, height / 2)
-  this.raduis = 20
-
-  this.render = function () {
-    translate(this.pos.x, this.pos.y)
-    triangle(-this.raduis, this.raduis, this.raduis, this.raduis, 0, -this.raduis) // สร้างรูปสามเหลี่ยม
-  }
-
+function keyPressed () {
+  if (keyCode === RIGHT_ARROW) ship.setRotation(0.1)
+  else if (keyCode === LEFT_ARROW) ship.setRotation(-0.1)
+  else if (keyCode === UP_ARROW) ship.boosting(true) // กด boost เมื่อกดลูกศรขึ้นเท่านั้น
 }
